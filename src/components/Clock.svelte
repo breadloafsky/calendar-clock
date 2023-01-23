7da3faf1544c5cfe8bc6c668288eac3e69bd5e27
ilgo6991 <ilya41pk@gmail.com>
2023-01-24 01:31:38 +1300

    added days of the year

--- src/components/Clock.svelte
+++ src/components/Clock.svelte
@@ -12,6 +12,7 @@
         start:number;       //  the start angle of the section
         end:number;         //  the end angle of the section
         color:string;
+        id:number;
     }
 
     interface Dial {
@@ -21,15 +22,18 @@
         r2:number;
         total:number;
         sections:Section[];
-        spiralCoeff?:number;
         fontSize:number;
+        hideProgress:boolean;  // hide progress color
+        spiralCoeff?:number;
     };
 
-    let milliseconds = 0;
+
+
 
     //  initialize dials
     function initTime(){
-        dials.seconds = <Dial>{
+        let time : { [key: string]: Dial } = {};
+        time.seconds = <Dial>{
             name:"seconds",
             r1:15,
             r2:18,
@@ -37,64 +41,117 @@
             color:"lightsteelblue",
             fontSize:7,
         };
-        dials.minutes = <Dial>{
+        time.minutes = <Dial>{
             name:"minutes",
             r1:18,
-            r2:22,
+            r2:21,
             total:60,
             color:"cornflowerblue",
             fontSize:8,
         };
-        dials.hours = <Dial>{
+        time.hours = <Dial>{
             name:"hours",
-            r1:22,
-            r2:26,
+            r1:21,
+            r2:24,
             total:24,
             color:"dodgerblue",
             fontSize:10,
         };
         
-        Object.entries<Dial>(dials).forEach( ([k, d] ) => {
+        Object.entries<Dial>(time).forEach( ([k, d] ) => {
             let offset = 0;
-            d.sections =  [...Array(d.total).keys()].map(a=> {
+            d.sections =  [...Array(d.total).keys()].map((a,i)=> {
                 let step = 1;
                 let section = <Section>{
                     name: a+1+"",
                     start: ((offset)/d.total) * 360,
                     end: ((step+offset)/d.total) * 360,
-                    color:d.color
+                    color:d.color,
+                    id:i,
                 };
                 offset += step;
                 return section;
             });
         });
+        dials = {...dials, ...time};
+    }
+
+    function daysInYear(year:number){
+        return (((year % 100) != 0 || (year % 400) == 0) && !((year & 3) != 0)) ?  366 : 365;
+    }
+    function dayOfTheYear(){
+        const date = new Date();
+        var dayCount = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+        var mn = date.getMonth();
+        var dn = date.getDate();
+        var dayOfYear = dayCount[mn] + dn;
+        if(mn > 1 && daysInYear(date.getFullYear()) == 366) dayOfYear++;
+        return dayOfYear;
     }
 
     function initDays() {
-        const date = new Date();
+        
+
+        const date = new Date(new Date().getFullYear(), 0, 1);
         //new Date(date.getFullYear(), date.getMonth(), 0).getDate()
-        let daysInMonth = <Dial>{
-            name:"daysInMonth",
-            r1:30,
+        let days : { [key: string]: Dial } = {};
+        days.daysInYear = <Dial>{
+            name:"daysInYear",
+            r1:24,
             r2:40,
-            total: 126,
+            total: 378,
             color:"orange",
-            spiralCoeff:3,
+            spiralCoeff:9,
             fontSize:10,
+            //hideProgress:true,//remove later
         };
+        days.daysInWeek = <Dial>{
+            name:"daysInWeek",
+            r1:24,
+            r2:27,
+            total: 42,
+            color:"darkgray",
+            fontSize:10,
+            hideProgress:true,
+        }; 
+
+        
+     
+        Object.entries<Dial>(days).forEach( ([k, d] ) => {
             let offset = 0;
-            daysInMonth.sections =  [...Array(daysInMonth.total).keys()].map(a=> {
+
+            date.setDate(date.getDate()  - Math.round(days.daysInYear.total/2));
+            
+            d.sections =  [...Array(d.total).keys()].map((a,i)=> {
                 let step = 1;
+                let name = a+1+"";
+                let color = d.color;
+
+                if(k == "daysInWeek")
+                    name = ["Mo","Tu","We","Th","Fr","Sa","Su"][(offset+6)%7];
+                else
+                {
+                    name = date.getDate()+"";
+                    
+                    color =  ["orange","yellow"][date.getMonth()%2];//`hsl(${(date.getMonth()/12)*360}, 70%, 50%)`;
+                    date.setDate(date.getDate() + 1);
+                }
+                    
+            
                 let section = <Section>{
-                    name: a+1+"",
-                    start: ((offset)/(daysInMonth.total/daysInMonth.spiralCoeff)) * 360,
-                    end: ((step+offset)/(daysInMonth.total/daysInMonth.spiralCoeff)) * 360,
-                    color:daysInMonth.color
+                    name: name,
+                    start: ((offset)/(d.total/(d.spiralCoeff ?? 1))) * 360,
+                    end: ((step+offset)/(d.total/(d.spiralCoeff ?? 1))) * 360,
+                    color:color,
+                    id:i,
                 };
                 offset += step;
                 return section;
             });
-            dials.daysInMonth = daysInMonth;
+        });
+
+        dials = {...dials, ...days};
+      ;
     }
 
 
@@ -104,28 +161,33 @@
         seconds:0,
         minutes:0,
         hours:0,
-        daysInMonth:0,
+        daysInWeek:0,
+        daysInYear:0,
     }
     
 
     function updateCalendar()
     {
         const d = new Date();
+
+
+        
         dialPos.milliseconds = d.getMilliseconds() / 1000;
         dialPos.seconds = (d.getSeconds() + dialPos.milliseconds)/60;
         dialPos.minutes = (d.getMinutes() + dialPos.seconds)/60;
         dialPos.hours =  (d.getHours() + dialPos.minutes)/24;
-        dialPos.daysInMonth = (d.getDate() + dialPos.hours + 42) / 42;
+        dialPos.daysInYear = (dayOfTheYear() + dialPos.hours + Math.round(dials.daysInYear.total/2)) / 42;
+        dialPos.daysInWeek = (d.getDay() + dialPos.hours)/(7*6);
 
 
         Object.entries<number>(dialPos).forEach(([k,p]) =>{
-            dialPos[k] = p *360;
+            dialPos[k] =Math.round(800 * (p * 360))/800;
         });
         setTimeout(updateCalendar, 10);
     }
-
-    initTime();
     initDays();
+    initTime();
+    
     
     updateCalendar();
     
@@ -139,13 +201,13 @@
                     d={svgFunctions.arc( dial.r1, dial.r2,section.start,section.end, dial.spiralCoeff, dialPos[dial.name])} 
                     style="stroke:white;stroke-width:0.1%; 
                     {
-                    (section.end < dialPos[dial.name]) ?
-                        `fill:${section.color}` :
-                        `fill:lightgray; filter: opacity(40%)`
+                    (section.end < dialPos[dial.name] || dial.hideProgress) ?
+                        `fill:${section.color};` :
+                        `fill:#e6e6e6; `
                     }"
                     transform={` translate(50 50)  rotate(${ section.start + 180 - dialPos[dial.name]}) `}     
                 />
-                {#if section.start < dialPos[dial.name] && section.end > dialPos[dial.name]}
+                {#if section.start < dialPos[dial.name] && section.end > dialPos[dial.name] && ! dial.hideProgress}
                 <path
                     d={svgFunctions.arc( dial.r1, dial.r2 ,section.start,dialPos[dial.name], dial.spiralCoeff, dialPos[dial.name])} 
                     style="stroke:white;stroke-width:0.1%; fill:{section.color}; filter: opacity(40%);"
@@ -153,7 +215,7 @@
                 />
                 {/if}
                     <path
-                        id={`path-${dial.name+":"+section.name}`}
+                        id={`path-${dial.name+":"+section.id}`}
                         d={svgFunctions.textArc(dial.r1, dial.r2, section.start,section.end, dial.spiralCoeff, dialPos[dial.name])}  
                         transform={`translate(50 50) rotate(${(section.start) + 180 - dialPos[dial.name]}) `}
                         fill=none
@@ -163,10 +225,10 @@
                         dominant-baseline="middle" 
                     >
                         <textPath
-                            startOffset={` ${ (((dial.r2 + dial.r1)/2) * (section.end-section.start) * (Math.PI/180))/2  - (0.13 * dial.fontSize * (section.name.length))/3 }`} 
+                            startOffset={` ${ svgFunctions.arcLength(dial.r1, dial.r2, section.start,section.end, dial.spiralCoeff, dialPos[dial.name])/2  - (0.13 * dial.fontSize * (section.name.length))/3 }`} 
                             font-size={dial.fontSize + "%"}
                             style="stroke-width: {dial.fontSize/25}%;"
-                            href={`#path-${dial.name+":"+section.name}`}>
+                            href={`#path-${dial.name+":"+section.id}`}>
                                 {section.name}
                         </textPath>
                         
--- src/utils/svgFunctions.ts
+++ src/utils/svgFunctions.ts
@@ -10,7 +10,81 @@ export const svgFunctions = {
         if(spiralCoeff)
         {
             let radiusBand = (radius-innerRadius);  // the overall width of the spiral
+            let sectionWidth = radiusBand/(spiralCoeff+1);
+            radiusBand -= sectionWidth;
+
+            let newInnerRadius = innerRadius + radiusBand/(360*spiralCoeff/angle2); 
+            let newRadius = newInnerRadius + sectionWidth;
+            
+            innerRadius2 = innerRadius + radiusBand/(360*spiralCoeff/angle1);
+            radius2 = innerRadius2 + sectionWidth;
+            
+            innerRadius = newInnerRadius;
+            radius = newRadius;
+        } 
+        
+
+        path += `M ${0} ${radius2} `
+        path += `A ${radius} ${radius}, 0, ${degree >= 180 ? 1 : 0}, 1, ${Math.sin(rad)*radius} ${Math.cos(rad)*radius} `;
+
+        path += `L ${Math.sin(rad)*innerRadius} ${Math.cos(rad)*innerRadius} `
+        path += `A ${innerRadius} ${innerRadius}, 0, ${degree >= 180 ? 1 : 0}, 0, ${0} ${innerRadius2} `;
+        path += `L 0 ${innerRadius2} `;
+       
+        path += "z";
+        return path;
+    },
+    textArc:function(innerRadius:number, radius:number, angle1:number, angle2:number, spiralCoeff:number = null, dialPosition:number = null){
+        let path = "";
+        let degree = angle2 - angle1;
+        degree = degree > 360 ? 360 : degree;
+        let rad = -degree * (Math.PI/180);
+
+        if(spiralCoeff)
+        {
+            let radiusBand = (radius-innerRadius);  // the overall width of the spiral
+            let sectionWidth = radiusBand/(spiralCoeff+1);
+            radiusBand -= sectionWidth;
+            radius = (innerRadius + radiusBand/(360*spiralCoeff/((angle1+angle2)/2))) + sectionWidth/2;
+        }
+        else
+            radius = (radius + innerRadius)/2;
+        
+
+
+        path += `M 0 ${radius} `;
+        path += `A ${radius} ${radius}, 0, ${degree >= 180 ? 1 : 0}, 1, ${Math.sin(rad)*radius} ${Math.cos(rad)*radius} `;
+        return path;
+    },
+    arcLength:function(innerRadius:number, radius:number, angle1:number, angle2:number, spiralCoeff:number = null, dialPosition:number = null)
+    {
+        if(spiralCoeff)
+        {
+            let radiusBand = (radius-innerRadius);  // the overall width of the spiral
+            let sectionWidth = radiusBand/(spiralCoeff+1);
+            radiusBand -= sectionWidth;
+            radius = (innerRadius + radiusBand/(360*spiralCoeff/((angle1+angle2)/2))) + sectionWidth/2;
+        }
+        else
+            radius = (radius + innerRadius)/2;
+        return  radius * (angle2-angle1) * (Math.PI/180);        
+    }
+}; 
+
+/* export const svgFunctions = {
+    arc:function(innerRadius:number, radius:number, angle1:number, angle2:number, spiralCoeff:number = null, dialPosition:number = null){
+        let path = "";
+        let degree = angle2 - angle1;
+        degree = degree > 360 ? 360 : degree;
+        let rad = -degree * (Math.PI/180);
+        let radius2 = radius;
+        let innerRadius2 = innerRadius;
+
+        if(spiralCoeff)
+        {
+            let radiusBand = (radius-innerRadius);  // the overall width of the spiral
             let sectionWidth = radiusBand/spiralCoeff;
+            
             
             let newInnerRadius = innerRadius + radiusBand/(360*spiralCoeff/angle2); 
             let newRadius = newInnerRadius + sectionWidth;
@@ -54,4 +128,16 @@ export const svgFunctions = {
         path += `A ${radius} ${radius}, 0, ${degree >= 180 ? 1 : 0}, 1, ${Math.sin(rad)*radius} ${Math.cos(rad)*radius} `;
         return path;
     },
-}; 
+    arcLength:function(innerRadius:number, radius:number, angle1:number, angle2:number, spiralCoeff:number = null, dialPosition:number = null)
+    {
+        if(spiralCoeff)
+        {
+            let radiusBand = (radius-innerRadius);
+            let sectionWidth = radiusBand/spiralCoeff;
+            radius = (innerRadius + radiusBand/(360*spiralCoeff/((angle1+angle2)/2))) + sectionWidth/2;
+        }
+        else
+            radius = (radius + innerRadius)/2;
+        return  radius * (angle2-angle1) * (Math.PI/180);        
+    }
+};  */
